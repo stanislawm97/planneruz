3995086c2a34e320a165c544640262f9b3ad43e8
Stanislaw Mol <stanislawmol97@gmail.com>
2021-01-02 17:17:16 +0100

    Narazie wszystko działa, tylko syf w kodzie.

--- src/main/java/com/planneruz/controller/HomeServlet.java
+++ src/main/java/com/planneruz/controller/HomeServlet.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Servlet implementation class HomeServlet
@@ -20,12 +19,7 @@ public class HomeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String path = request.getServletPath();
         if (path.equals("/")) {
-            List<Article> articles = articleDao.getAllArticles();
-            request.setAttribute("title", "Title of our application");
-            request.setAttribute("articles", articles);
-            request.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);
-        } else if (path.equals("/welcome")) {
-            request.getRequestDispatcher("/WEB-INF/view/welcome.jsp").forward(request, response);
+            request.getRequestDispatcher("/WEB-INF/view/calendar.jsp").forward(request, response);
         }
     }
 
